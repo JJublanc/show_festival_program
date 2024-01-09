c5fdcd11574cac11a3e05fd7dc28a9c3d0ddc78a
jjublanc <johan.jublanc@gmail.com>
2024-01-09 04:18:16 +0100

    fix(scripts): bug display swipper slides

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,5 +1,3 @@
-import data from "../data/etrange_festival_2022.json" assert {type: "json"};
-
 var calendarEl = document.getElementById('calendar');
 var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth'
@@ -8,13 +6,23 @@ var calendar = new FullCalendar.Calendar(calendarEl, {
 document.addEventListener('DOMContentLoaded', function () {
     calendar.render();
     calendar.setOption('aspectRatio', 1.5)
-    append_swiper_slides(data)
+    axios.get('http://localhost:3000/api/shows')
+        .then(response => {
+            // Stockez les données de la réponse dans l'objet myData
+            append_swiper_slides(response.data);
+            // Vous pouvez maintenant accéder aux données via myData.data
+        })
+        .catch(error => {
+            // Gérez les erreurs ici
+            console.error('Erreur lors de la requête:', error);
+        });
     def_swiper();
 });
 
 function append_swiper_slides(data) {
     for (let i = 0; i < data.length; i++) {
-        append_a_slide(data[i])
+        append_a_slide(data[i]);
+        console.log('Itération de la boucle :', i);
     }
 }
 
@@ -23,25 +31,25 @@ function append_a_slide(show) {
     var swiper_slide = document.createElement('div');
     swiper_slide.classList.add('swiper-slide');
     let content =
-        '<img src=' + show.ImageURL + '>' +
-        '<h3>' + show.Title + '</h3>'
-    for (let i = 0; i < show.Seances.length; i++) {
-        let event_id = show.id + "_" + show.Seances[i].id
+        '<img src=' + show.imageURL + '>' +
+        '<h3>' + show.title + '</h3>'
+    for (let i = 0; i < show.sessions.length; i++) {
+        let event_id = show._id + "_" + show.sessions[i]._id
         content +=
             '<label class="checkbox-container">' +
             '<div>' +
             '<input type="checkbox" id=' + event_id + '>' +
-            show.Seances[i].description +
+            show.sessions[i].date + " " + show.sessions[i].time + " " + show.sessions[i].location
             '</label>'
     }
     swiper_slide.innerHTML = content;
     swiper.appendChild(swiper_slide);
-    for (let i = 0; i < show.Seances.length; i++) {
-        let event_id = show.id + "_" + show.Seances[i].id
+    for (let j = 0; j < show.sessions.length; j++) {
+        let event_id = show._id + "_" + show.sessions[j]._id
         let event = {
             id: event_id,
-            title: show.Title, // Modify this to match your event data
-            date: show.Seances[i].start, // Modify this to match the event start date
+            title: show.title, // Modify this to match your event data
+            date: show.sessions[j].date, // Modify this to match the event start date
         };
         let checkbox = document.getElementById(event_id)
         add_event_listener(checkbox, event)
@@ -64,14 +72,15 @@ function add_event_listener(checkbox, event) {
 }
 
 
-let download_program_button = document.getElementById('download_program_button')
-download_program_button.addEventListener('click', download_program)
+// let download_program_button = document.getElementById('download_program_button')
+// download_program_button.addEventListener('click', download_program)
 function def_swiper() {
 
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 3,
         centeredSlides: true,
         spaceBetween: 30,
+        observer:true,
         pagination: {
             el: ".swiper-pagination",
             type: "fraction",
