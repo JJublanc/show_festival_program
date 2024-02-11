d22c854287012b9498c191143a52ba14a7cc71ec
jjublanc <johan.jublanc@gmail.com>
2024-02-11 20:04:38 +0100

    fix(deploy): use fixie add-on on Heroku to get a unique IP that can b whitelisted on MongoDB

--- backend/app.js
+++ backend/app.js
@@ -6,19 +6,32 @@ const festivalRoutes = require('./routes/festival');
 const userRoutes = require('./routes/user');
 require('dotenv').config();
 app.use(express.json());
+const url = require('url');
+const fixieUrl = url.parse(process.env.FIXIE_SOCKS_HOST);
+const fixieAuth = fixieUrl.auth.split(':');
 
+console.log(process.env.FIXIE_URL);
+console.log(fixieUrl.hostname);
+console.log(fixieUrl.port);
+console.log(fixieAuth);
 mongoose.connect(process.env.MONGODB_URI,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+    {
+        proxyHost: fixieUrl.hostname,
+        proxyPort: fixieUrl.port,
+        proxyUsername: 'fixie',
+        proxyPassword: fixieAuth[0],
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch((reason)  => console.log('Connexion à MongoDB échouée !', reason));
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
 
